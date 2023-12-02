2e79fbae3b137ab7d53e2a63fd7ad710633c3949
khuongnm.dev <khuongnm.dev@gmail.com>
2023-12-03 05:54:42 +0700

    Move transaction list to component

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { CardMaskPipe } from './pipes/card-mask.pipe';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 
 const SHARE_COMPONENTS = [
   CardComponent,
   NavigationComponent,
+  TransactionListComponent,
   CardMaskPipe
 ]
 @NgModule({
